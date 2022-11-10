dbd61bd6a9e9ed49fca4fd96e56f70c8678a8239
fatihabdi <dotaawtf23@gmail.com>
2022-11-10 23:27:33 +0700

    feat/add get home category

--- finpro-BEAI-sc/app/Http/Controllers/HomeController.php
+++ finpro-BEAI-sc/app/Http/Controllers/HomeController.php
@@ -3,14 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
+use Illuminate\Http\Response;
 
 class HomeController extends Controller
 {
-    public function get_banner() {
+    public function get_banner()
+    {
         return "Halaman Get Banner";
     }
 
-    public function get_category() {
-        return "Halaaman Get Category";
+    public function get_category()
+    {
+        $categories = Category::get()->all();
+        $data = [];
+        foreach ($categories as $category) {
+            $json = new Response([
+                'id' => $category->id,
+                'title' => $category->category_name
+            ]);
+            array_push($data, $json->original);
+        };
+        return new Response([
+            'data' => $data
+        ]);
     }
 }
