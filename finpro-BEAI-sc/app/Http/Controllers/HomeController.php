80ee2dc5dcb90a465509e4e3c470e32611cece5a
fatihabdi <dotaawtf23@gmail.com>
2022-11-10 23:28:45 +0700

    feat/ add try catch

--- finpro-BEAI-sc/app/Http/Controllers/HomeController.php
+++ finpro-BEAI-sc/app/Http/Controllers/HomeController.php
@@ -15,17 +15,29 @@ class HomeController extends Controller
 
     public function get_category()
     {
-        $categories = Category::get()->all();
-        $data = [];
-        foreach ($categories as $category) {
-            $json = new Response([
-                'id' => $category->id,
-                'title' => $category->category_name
+        try {
+            $categories = Category::get()->all();
+            $data = [];
+            foreach ($categories as $category) {
+                $json = new Response([
+                    'id' => $category->id,
+                    'title' => $category->category_name
+                ]);
+                array_push($data, $json->original);
+            };
+            return new Response([
+                'data' => $data
             ]);
-            array_push($data, $json->original);
-        };
-        return new Response([
-            'data' => $data
-        ]);
+        } catch (\Throwable $th) {
+            return new Response([
+                'message' => 'Internal Server Error',
+                'error' => $th->getMessage()
+            ], 500);
+        } catch (\Exception $e) {
+            return new Response([
+                'message' => 'failed',
+                'error' => $e->getMessage()
+            ], 409);
+        }
     }
 }
