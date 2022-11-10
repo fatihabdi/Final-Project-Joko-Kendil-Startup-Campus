1931cca15b35e7955501bd63c2f442ef9b7ac037
fatihabdi <dotaawtf23@gmail.com>
2022-11-10 19:07:15 +0700

    feat/add create products API and create category API

--- finpro-BEAI-sc/app/Http/Controllers/AdminController.php
+++ finpro-BEAI-sc/app/Http/Controllers/AdminController.php
@@ -3,22 +3,85 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Product;
 
 class AdminController extends Controller
 {
-    public function get_order() {
+    public function get_order()
+    {
         return "Halaman Get Order";
     }
 
-    public function create_product() {
-        return "Halaman Create Product";
+    public function create_product(Request $request)
+    {
+        try {
+            $request->validate([
+                'product_name' => 'required|string|max:255',
+                'description' => 'required|string|max:255',
+                'is_new' => 'required',
+                'category' => 'required',
+                'price' => 'required'
+            ]);
+
+            $product = Product::create([
+                'product_name' => $request->product_name,
+                'description' => $request->description,
+                'is_new' => $request->is_new,
+                'category' => $request->category,
+                'price' => $request->price
+            ]);
+            $product->save();
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Product created successfully',
+                'data' => $product
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Internal Server Error',
+                'error' => $th->getMessage()
+            ], 500);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'failed',
+                'error' => $e->getMessage()
+            ], 409);
+        }
     }
 
-    public function create_category() {
-        return "Halaman Create Category";
+    public function create_category(Request $request)
+    {
+        try {
+            $request->validate([
+                'category_name' => 'required|string',
+            ]);
+
+            $category = Category::create([
+                'category_name' => $request->category_name
+            ]);
+            $category->save();
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Category created successfully',
+                'data' => $category
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Internal Server Error',
+                'error' => $th->getMessage()
+            ], 500);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'failed',
+                'error' => $e->getMessage()
+            ], 409);
+        }
     }
 
-    public function get_total_sales() {
+    public function get_total_sales()
+    {
         return "Halaman Get Total Sales";
     }
 }
